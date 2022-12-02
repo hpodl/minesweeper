4debe727e1bea627aab883acf1a76e1cd34e2c17
hpodl <hubert.podlewski.stud@pw.edu.pl>
2022-12-02 13:29:27 +0100

    Added few more tests

--- tests/GameBoardTests.cpp
+++ tests/GameBoardTests.cpp
@@ -1,6 +1,12 @@
 #include <catch2/catch_all.hpp>
 #include "GameBoard.hh"
 
+#include <algorithm>
+
+bool operator==(const Point& p1, const Point& p2) {
+    return p1.x == p2.x && p1.y == p2.y;
+}
+
 class FieldVecTestable : public _FieldVector {
     void populate(Points mines) {
         for(auto point : mines) {
@@ -18,3 +24,66 @@ TEST_CASE("GameBoard initializes with correct size.", "[GameBoard, size, initial
     unsigned int width(12), height(14);
     REQUIRE(GameBoard(width, height, 30).size() == width*height);
 }
+
+TEST_CASE("GameBoard correctly reveals recursively.", "[GameBoard, reveal, recursive]") {
+    unsigned int width(12), height(14);
+    REQUIRE(GameBoard(width, height, 30).size() == width*height);
+}
+
+
+
+TEST_CASE("neighbourCoords return valid coordinates", "[FieldVector, neighbourCoords]") {
+    auto fields = _FieldVector(20,20,0);
+    Point point(2,2);
+    Points actualCoords {
+        {1,1}, {1,2}, {1,3},
+        {2,1}, {2,2}, {2,3},
+        {3,1}, {3,2}, {3,3}
+    };
+
+    Points calculated = fields.neighbourCoords(point);
+
+    REQUIRE(std::is_permutation(actualCoords.begin(), actualCoords.end(), calculated.begin()) == true);
+}
+
+TEST_CASE("neighbourCoords return valid coordinates on the upper edge", "[FieldVector, neighbourCoords]") {
+    auto fields = _FieldVector(20,20,0);
+    Point point(0,2);
+    Points actualCoords {
+        {0,1}, {0,2}, {0,3},
+        {1,1}, {1,2}, {1,3}
+    };
+
+    Points calculated = fields.neighbourCoords(point);
+
+
+    REQUIRE(std::is_permutation(actualCoords.begin(), actualCoords.end(), calculated.begin()) == true);
+}
+
+
+TEST_CASE("neighbourCoords return valid coordinates in the upper-left corner", "[FieldVector, neighbourCoords]") {
+    auto fields = _FieldVector(20,20,0);
+    Point point(0,0);
+    Points actualCoords {
+        {0,0}, {0,1},
+        {1,0}, {1,1}
+    };
+
+    Points calculated = fields.neighbourCoords(point);
+
+    REQUIRE(std::is_permutation(actualCoords.begin(), actualCoords.end(), calculated.begin()) == true);
+}
+
+TEST_CASE("neighbourCoords return valid coordinates in the lower-right corner", "[FieldVector, neighbourCoords]") {
+    auto fields = _FieldVector(10, 12, 0);
+    Point point(9,11);
+    Points actualCoords {
+        {8, 10}, {9, 10},
+        {8, 11}, {9, 11}
+    };
+
+    Points calculated = fields.neighbourCoords(point);
+
+    REQUIRE(std::is_permutation(actualCoords.begin(), actualCoords.end(), calculated.begin()) == true);
+}
+
